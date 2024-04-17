bdb71701d52e0aea34175e5f64915c9f86a7672a
camille <camillecalas@hotmail.fr>
2024-04-17 19:23:41 +0200

    nav mis dans un composant

--- src/App.js
+++ src/App.js
@@ -3,54 +3,47 @@ import axios from 'axios';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Search from './components/Search';
 import Home from './components/Home';
+import Nav from './components/Nav';
 import SearchResults from './SearchResults';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
-  const navigate = useNavigate();
+	const [movies, setMovies] = useState([]);
+	const [searchResults, setSearchResults] = useState([]);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_MOVIE_API}`);
-        setMovies(response.data.results);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+		try {
+			const response = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_MOVIE_API}`);
+			setMovies(response.data.results);
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  const handleSearch = async (query) => {
-    try {
-      const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API}&query=${query}`);
-      setSearchResults(response.data.results);
-      // Naviguer vers une nouvelle URL pour afficher les résultats de recherche
-      navigate('/search-results');
-    } catch (error) {
-      console.error('Error searching:', error);
-    }
-  };
+  	const handleSearch = async (query) => {
+		try {
+		const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API}&query=${query}`);
+		setSearchResults(response.data.results);
+		// Naviguer vers une nouvelle URL pour afficher les résultats de recherche
+		navigate('/search-results');
+		} catch (error) {
+		console.error('Error searching:', error);
+		}
+  	};
 
-  return (
-    <div>
-      <nav>
-        <div>
-          <Link to="/">
-            <h1>LOGO</h1>
-          </Link>
-        </div>
-        <Search onSearch={handleSearch} />
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home  />} />
-        <Route path="/search-results" element={<SearchResults searchResults={searchResults} />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+		<Nav handleSearch={handleSearch} />
+		<Routes>
+			<Route path="/" element={<Home  />} />
+			<Route path="/search-results" element={<SearchResults searchResults={searchResults} />} />
+		</Routes>
+		</div>
+	);
 }
 
 
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import MovieDetails from './MovieDetails';
 
 function SearchResults({ searchResults }) {
+	console.log(searchResults)
 	return (
 	  <div>
 		{searchResults.length > 0 ? (
 		  searchResults.map((movie) => (
 			<img
-			  key={movie.id}
-			  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-			  alt={movie.title}
+			  	key={movie.id}
+			  	src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+			  	alt={movie.title}
 			/>
 		  ))
 		) : (
